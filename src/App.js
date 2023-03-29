1876496bd42a69392c79e22f41de58804454e8b3
ajaytyagi311 <musicfan3113.com>
2023-03-30 00:54:53 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Cards from "./cards.js";
+import arr from "./array";
+import react, { useEffect, useState } from "react";
+let first = 1,
+last = 3;
+const arr1 = arr(first, last);
 
 function App() {
+
+  const [A1ind, setA1ind] = useState(-1);
+  
+  useEffect(() => {
+    console.log("aiind changed", A1ind);
+  }, [A1ind]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cards
+        arr1={arr1}
+        last={last}
+        first={first}
+        A1ind={A1ind}
+        setA1ind={setA1ind}
+      />
     </div>
   );
 }
